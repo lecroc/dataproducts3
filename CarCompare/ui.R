dcba2242ad733a4d68556eeee28f46ae70e21ed1
lecroc <clecrone@compasspoint-media.com>
2017-11-13 21:20:40 -0600

    Work from 11-13-17

--- CarCompare/ui.R
+++ CarCompare/ui.R
@@ -1,33 +1,17 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+d2<-read.table("C:/Coursera/DevelopingDataProducts/dataproducts3/d2.Rda")
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+pageWithSidebar(
+  headerPanel('Vehicle Fuel Efficiency'),
+  sidebarPanel(
+    selectInput('class', 'Select Vehicle Class', levels(d2$class)),
+    numericInput('Year', 'Model Year', 1984,
+                 min = 1984, max = 2018)
+  ),
+  mainPanel(
+    h3("Selected Values:"),
+    textOutput("vclass"),
+    textOutput("Year")
   )
-))
+)
